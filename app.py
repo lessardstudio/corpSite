2c74fad137318995e0a8beb62b13e0ae795cf0f7
lessardstudio <gost.ks2@gmail.com>
2026-01-22 11:29:23 +0300

    feat(ip-checker): enhance IP detection and add debug headers view
    - Improve IP detection logic to handle VPN/proxy cases better
    - Add X-Forwarded-For IP display and debug headers toggle
    - Update UI styling and add visual indicators for VPN connections

--- app.py
+++ app.py
@@ -14,26 +14,35 @@ def get_server_public_ip():
 
 @app.route('/')
 def index():
-    # Visitor's IP as seen by the server (this is the Global/VPN IP if tunneled)
+    # Visitor's IP as seen by the server
+    # When using Hysteria2/Xray on the same server, this will often be 127.0.0.1
+    # because the VPN server acts as a local proxy.
     visitor_ip = request.remote_addr
     
-    # Check for X-Forwarded-For header in case of proxy
+    # Check for X-Forwarded-For header in case of proxy (Nginx etc)
+    forwarded_ip = None
     if request.headers.getlist("X-Forwarded-For"):
-        visitor_ip = request.headers.getlist("X-Forwarded-For")[0]
+        forwarded_ip = request.headers.getlist("X-Forwarded-For")[0]
 
     # Server's Public IP (Global)
     server_global_ip = get_server_public_ip()
     
-    # Server's Local IP (Container IP)
+    # Server's Local IP
     hostname = socket.gethostname()
     server_local_ip = socket.gethostbyname(hostname)
+
+    # Get all headers for debug
+    headers = dict(request.headers)
 
     return render_template(
         'index.html', 
         visitor_ip=visitor_ip, 
+        forwarded_ip=forwarded_ip,
         server_global_ip=server_global_ip,
-        server_local_ip=server_local_ip
+        server_local_ip=server_local_ip,
+        headers=headers
     )
 
 if __name__ == '__main__':
+    # Listen on all interfaces
     app.run(host='0.0.0.0', port=80)
